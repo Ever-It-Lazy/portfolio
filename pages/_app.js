f5b9f768218268726a4df5f1c691ed530753ea6f
Everett Lindsay <everettsjunk@gmail.com>
2023-02-14 13:06:50 -0500

    custom font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Baskervville } from '@next/font/google'
+
+const basker = Baskervville({
+	weight: '400',
+	subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={basker.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
