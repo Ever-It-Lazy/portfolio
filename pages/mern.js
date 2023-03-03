d4b2105cefeae4d50c9b75cb71584b6435b4efb2
Everett Lindsay <everettsjunk@gmail.com>
2023-03-03 09:44:57 -0500

    relocated the MERN/Notezipper tutorial from Heroku to Render

--- pages/mern.js
+++ pages/mern.js
@@ -29,7 +29,7 @@ export default function Mern({ show }) {
 				</ul>
 				<ul className="flex items-center mt-4 space-x-1">
 					<li>
-						<a href="https://mern-tutorial-2.herokuapp.com/" className="link-underline text-sm md:text-lg" target="_blank">View Site</a>
+						<a href="https://notezipper-rebuild.onrender.com/" className="link-underline text-sm md:text-lg" target="_blank">View Site</a>
 					</li>
 					<li>
 						<Link href="https://github.com/Ever-It-Lazy/Notezipper-rebuild" target="_blank">
